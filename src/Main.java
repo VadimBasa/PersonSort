654d68508ea535daf30f26dd6418fa367ab9f65e
Vadim Basalaev <basalaev.vad@yandex.ru>
2022-08-24 22:31:54 +0400

    Add split surname

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ class Main {
         List<Person> people2 = Arrays.asList(
                 new Person("Николай", "Валуеваев", 52),
                 new Person("Виктор", "Цой", 32),
-                new Person("Михаил", "Круглогодич", 40),
-                new Person("Брюс", "Лиy", 30),
-                new Person("Константин", "Дзю", 51)
+                new Person("Наталья", "Васильева-Бернард-Шоу", 40),
+                new Person("Брюс", "Миклухо-Маклай-Маклаевич", 30),
+                new Person("Константин", "Салтыков-Щедрин", 51)
         );
-        Collections.sort(people2, new PersonSurnameComparator(6));
+        Collections.sort(people2, new PersonSurnameComparator(2));
         System.out.println(people2.toString());
 
     }
--- src/PersonSurnameComparator.java
+++ src/PersonSurnameComparator.java
@@ -9,14 +9,14 @@ public class PersonSurnameComparator implements Comparator<Person> {
 
     @Override
     public int compare(Person o1, Person o2) {
-
-        if (o1.surname.length() != o2.surname.length()) {
-            return o2.surname.length() - o1.surname.length();
-        } else if (o1.surname.length() > surnameMax) {
+        String[] surnameSplitO1 = o1.surname.split("-");
+        String[] surnameSplitO2 = o2.surname.split("-");
+        if (surnameSplitO1.length != surnameSplitO2.length) {
+            return surnameSplitO2.length - surnameSplitO1.length;
+        } else if (surnameSplitO1.length > surnameMax) {
             return o2.age - o1.age;
         } else {
             return o2.age - o1.age;
         }
     }
 }
-
