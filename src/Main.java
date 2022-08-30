6fa5810215d36cd7501f36a09fbac41f3484e1a7
Vadim Basalaev <basalaev.vad@yandex.ru>
2022-08-31 00:01:17 +0400

    Add lambda without class comparator

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,21 @@ class Main {
                 new Person("Брюс", "Миклухо-Маклай-Маклаевич", 30),
                 new Person("Константин", "Салтыков-Щедрин", 51)
         );
-        Collections.sort(people2, new PersonSurnameComparator(2));
+        Comparator<Person> comparator;
+        comparator = Main::compare;
+        Collections.sort(people2, comparator);
         System.out.println(people2.toString());
+    }
 
+    private static int compare(Person o1, Person o2) {
+        String[] surnameSplitO1 = o1.surname.split("-");
+        String[] surnameSplitO2 = o2.surname.split("-");
+        if (surnameSplitO1.length != surnameSplitO2.length) {
+            return surnameSplitO2.length - surnameSplitO1.length;
+        } else if (surnameSplitO1.length > 2) {
+            return o2.age - o1.age;
+        } else {
+            return o2.age - o1.age;
+        }
     }
 }
